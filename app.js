07d6b64bf35b39c1b79f01402357af036f718ec4
ZLiu-Ubuntu-PC <lavandejoey@outlook.com>
2025-02-03 10:45:11 +0100

    20250203 Fix the mobile screen display of about-me.pug; update and contact.pug; add log to indicate redis connection; update the source of some image resources.

--- app.js
+++ app.js
@@ -44,9 +44,11 @@ app.use(cookieParser());
 // Initialize Redis client
 const redis = new Redis({
     port: process.env.REDIS_LOCAL_PORT,
-    host: "127.0.0.1",
+    host: process.env.REDIS_LOCAL_HOST,
     username: process.env.REDIS_USER,
     password: process.env.REDIS_PASSWORD
+}).on("connect", () => {
+    console.log("Connected to Redis");
 });
 
 // Middleware: Session handling (must come before csurf)
